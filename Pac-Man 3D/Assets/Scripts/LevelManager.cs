e6950c411ec792bbc7242424a66e5c25a3595199
Jakub Grodzki <kubagrodzki@gmail.com>
2017-10-26 15:48:30 +0200

    Zmiany.

---
+++ Pac-Man 3D/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject floor;
+	public GameObject wall;
+	public int mapWidth;
+	public int mapHeight;
+
+	// Use this for initialization
+	void Start () {
+		CreateLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void CreateLevel(){
+		Instantiate (floor);
+		Instantiate (wall);
+
+		for(int x = 0; x < mapWidth; x++){
+			for(int y = 0; y < mapHeight; y++){
+
+			}
+		}
+				
+	}
+}
